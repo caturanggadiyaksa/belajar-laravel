2c7e36e4187bc346c232e3a93a85d96139dd4fae
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-01-02 12:09:35 +0700

    Controller laravel

---
+++ Bab-4/README.md
@@ -0,0 +1,13 @@
+BAB 4 
+CONTROLLER
+
+
+
+jika terdapat eror dalam route / target class tidak ditemukan maka
+
+tambahkan code seperti dibawah ini pada file app/Providers/RouteServiceProvider.php
+protected $namespace = 'App\\Http\\Controllers';
+
+
+
+Mengirim (Passing) Data dari Controller ke View
---
+++ Bab-4/belajar_controller_laravel/resources/views/biodata.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tutorial Passing Data Controller ke View</title>
+</head>
+<body>
+    <h1>Tutorial Laravel</h1>
+
+    <a href="">caa.web.id</a>
+</body>
+</html>
