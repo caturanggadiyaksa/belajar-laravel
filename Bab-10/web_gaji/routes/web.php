501f241d76e1f742adc49bd79b8af46c1a472447
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-01-23 18:49:46 +0700

    add route hapus

--- Bab-10/web_gaji/routes/web.php
+++ Bab-10/web_gaji/routes/web.php
@@ -25,3 +25,4 @@ Route::get('/pegawai/tambah', 'App\Http\Controllers\PegawaiController@tambah');
 Route::post('/pegawai/store', 'App\Http\Controllers\PegawaiController@store');
 Route::get('/pegawai/edit/{id}', 'App\Http\Controllers\PegawaiController@edit');
 Route::post('/pegawai/update', 'App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}', 'App\Http\Controllers\PegawaiController@hapus');
