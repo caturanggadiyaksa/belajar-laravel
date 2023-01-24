42acf57dd4926b5377de26f083c69b7ca611c129
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-01-24 08:05:18 +0700

    bab 12 pagination

---
+++ Bab-12/web_gaji/resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menampilkan data dari database</title>
+</head>
+<body>
+    <a href="/pegawai/tambah">+ Tambah Pegawai Baru</a>
+    <br>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Jabatan</th>
+            <th>Umur</th>
+            <th>Alamat</th>
+            <th>Opsi</th>
+        </tr>
+        @foreach ($pegawai as $p)
+        <tr>
+            <td>{{ $p->nama }}</td>
+            <td>{{ $p->jabatan }}</td>
+            <td>{{ $p->umur }}</td>
+            <td>{{ $p->alamat }}</td>
+            <td>
+                <a href="/pegawai/edit/{{ $p->id }}">Edit</a>
+                <a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
+            </td>
+            
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
---
+++ Bab-12/web_gaji/resources/views/tambah.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>tambah</title>
+</head>
+<body>
+    <h3>Data Pegawai</h3>
+    <br>
+
+    <a href="/pegawai">Kembali</a>
+
+    <br><br>
+
+    <form action="/pegawai/store" action="post">
+        
+        {{ csrf_field() }}
+       
+        nama <input type="text" name="nama">
+        <br>
+        Jabatan <input type="text" name="jabatan">
+        <br>
+        umur <input type="number" name="umur">
+        <br>
+        Alamat <textarea name="alamat" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="Simpan Data">
+    </form>
+</body>
+</html>
