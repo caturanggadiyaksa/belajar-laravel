36a1160a8d0658e4fe8c52550606b9eb0b5fa5cc
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-02-13 20:51:57 +0700

    edit id pada route update

--- Bab-19/sisfodik/routes/web.php
+++ Bab-19/sisfodik/routes/web.php
@@ -21,5 +21,5 @@ Route::get('/pegawai', 'PegawaiController@index');
 Route::post('/pegawai/store', 'PegawaiController@store');
 Route::get('/pegawai/tambah', 'PegawaiController@tambah');
 Route::get('/pegawai/edit/{id}', 'PegawaiController@edit');
-Route::put('/pegawai/update/{$id}', 'PegawaiController@update');
+Route::put('/pegawai/update/{id}', 'PegawaiController@update');
 Route::get('pegawai/hapus/{id}', 'PegawaiController@delete');
