7607ce41b4e802b1635d101342c386d58aa0ef16
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-02-24 08:59:41 +0700

    membuat controller web

---
+++ Bab-21/sisfodik/app/Http/Controllers/WebController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    //
+    public function index() {
+        $artikel = Article::all();
+        return view('article', ['artikel' => $artikel]);
+
+    }
+}
